0d6405fe4b12498864e79cc3922c085efe0ded2d
Jennifer Dryden <jenndryden8@gmail.com>
2021-10-23 16:36:33 -0700

    Time: 28 ms (96.65%), Space: 14 MB (91.96%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,26 +1,19 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        visit = set()
-
-        while n not in visit:
-
-                #helper function
-                #add to set 
-            visit.add(n)
-            n = self.helperFunction(n)
-
-                #return true if 1
+        seen = {}
+        while True: 
+            if n in seen:
+                break
+            counter = 0
+            for i in range(len(str(n))):
+                counter += int(str(n)[i]) ** 2
+            seen[n] = 1
+            n = counter
+            counter = 0
             if n == 1:
                 return True
-
-                #otherwise return false
+                break
+            else:
+                continue
         return False
-        
-    def helperFunction(self, n: int) -> int:
-        count = 0 
-            
-        while n != 0:
-            digit = n % 10
-            count += digit ** 2
-            n = n // 10
-        return count
+                
