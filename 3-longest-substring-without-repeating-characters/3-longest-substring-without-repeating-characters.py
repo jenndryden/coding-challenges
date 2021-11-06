8289b990b2d4d879389200440f6e06a9a8f56555
Jennifer Dryden <jenndryden8@gmail.com>
2021-11-06 14:48:56 -0700

    Time: 48 ms (97.26%), Space: 14.3 MB (80.42%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -3,23 +3,21 @@ class Solution:
         
         sub = {}
         curr_sub_start = 0
-        curr_length = 0
-        longest = 0 
-        
-        if len(s) < 1:
-            return 0
-        
+        curr_sub_length = 0
+        longest = 0
+
         for i, letter in enumerate(s):
-            
-            if letter in sub and sub[letter] >= curr_sub_start:
-                curr_sub_start = sub[letter]+1
-                curr_length = i - sub[letter]
+
+            if letter in sub and sub[letter]>=curr_sub_start:
+                curr_sub_start = sub[letter] 
+                curr_sub_length = i - sub[letter]
                 sub[letter] = i
-            
-            else:
-                sub[letter] = i 
-                curr_length += 1
-                if curr_length > longest:
-                    longest = curr_length
-            
-        return(longest)        
+
+            else: 
+                curr_sub_length += 1
+                sub[letter] = i
+
+                if curr_sub_length > longest:
+                    longest = curr_sub_length 
+
+        return(longest)
