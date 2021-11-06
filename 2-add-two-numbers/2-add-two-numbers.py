55c4e9baa807092a4668a5561c412652c6fe5b7f
Jennifer Dryden <jenndryden8@gmail.com>
2021-11-06 12:49:35 -0700

    Time: 68 ms (81.21%), Space: 14.4 MB (12.15%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -4,32 +4,33 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         added = ListNode(val = (l1.val + l2.val) % 10)
-        carryover = (l1.val + l2.val) // 10
-        currentnode = added 
-        
-        while l1.next and l2.next:
+        carry_over = (l1.val + l2.val) // 10
+        current_node = added
+
+        while l1.next and l2.next: 
             l1 = l1.next
             l2 = l2.next
-            currentnode.next = ListNode(val = (carryover + l1.val + l2.val) % 10)
-            carryover = (carryover + l1.val + l2.val) // 10
-            currentnode = currentnode.next
-            
+            current_node.next = ListNode(val = (carry_over + l1.val + l2.val) % 10)
+            carry_over = (carry_over + l1.val + l2.val) // 10
+            current_node = current_node.next
+
         while l1.next:
             l1 = l1.next
-            currentnode.next = ListNode(val = (carryover + l1.val) % 10)
-            carryover = (carryover + l1.val) // 10
-            currentnode = currentnode.next
-        
+            current_node.next = ListNode(val = (carry_over + l1.val) % 10)
+            carry_over = (l1.val + carry_over) // 10
+            current_node = current_node.next
+
         while l2.next:
             l2 = l2.next
-            currentnode.next = ListNode(val = (carryover + l2.val) % 10)
-            carryover = (carryover + l2.val) // 10
-            currentnode = currentnode.next
-            
-        
-        if carryover > 0:
-            currentnode.next = ListNode(val = (1))
+            current_node.next = ListNode(val = (carry_over + l2.val ) % 10)
+            carry_over = (carry_over + l2.val) // 10
+            current_node = current_node.next
+
+        if carry_over>0:
+            current_node.next = ListNode(val = (1))
             
         return(added)
+            
