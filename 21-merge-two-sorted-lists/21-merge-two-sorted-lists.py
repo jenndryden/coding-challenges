65a73d091f7464121070ea593cfcc37e52a5fa49
Jennifer Dryden <jenndryden8@gmail.com>
2021-11-06 13:00:57 -0700

    Time: 32 ms (93.93%), Space: 14.2 MB (86.06%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -9,23 +9,19 @@ class Solution:
         tail = dummy
         
         while l1 and l2:
-            if l1.val < l2.val:
-                tail.next = l1
-                l1 = l1.next
-            
-            else: 
+            if l1.val > l2.val: 
                 tail.next = l2
                 l2 = l2.next
+            else:
+                tail.next = l1
+                l1 = l1.next
             tail = tail.next
-            
+        
         if l1:
             tail.next = l1
-            #l1 = l1.next
-            #tail = tail.next
             
-        elif l2:
+        if l2:
             tail.next = l2
-            #l2 = l2.next
-            #tail = tail.next
         
-        return dummy.next
+        return(dummy.next)
+            
